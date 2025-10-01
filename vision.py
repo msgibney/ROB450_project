74ff4146d038fcfc5d17d1656fdb615f9842a439
Raspberry PI <pi@pi.com>
2025-10-01 12:17:16 -0400

    tracks the centroid

--- vision.py
+++ vision.py
@@ -1,4 +1,5 @@
 import cv2
+import imutils
 
 print("initializing camera")
 cam = cv2.VideoCapture(0)
@@ -13,14 +14,31 @@ out = cv2.VideoWriter('output.mp4', fourcc, 30.0, (frame_width, frame_height))
 
 print("Finished Initializing")
 
-for i in range(150):
+for i in range(300):
     ret, frame = cam.read()
 
-    # Write the frame to the output file
-    out.write(frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    thresh = cv2.threshold(blurred, 80, 255, cv2.THRESH_BINARY)[1]
+    cnts, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 
-    # Display the captured frame
-    #cv2.imshow('Camera', frame)
+    coordinates = []
+    for c in cnts:
+        print(len(cnts))
+        M = cv2.moments(c)
+        if M["m00"] != 0:
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+        else:
+            cX, cY = 0, 0
+        #cv2.circle(image, (cX, cY), 1, (255, 255, 255), -1)
+        coordinates.append([cX,cY])
+        cv2.circle(frame, (cX, cY), 5, (255, 255, 255), -1)
+        cv2.putText(frame, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        out.write(frame)
+
+        print([cX, cY])
 
     # Press 'q' to exit the loop
     if cv2.waitKey(1) == ord('q'):
